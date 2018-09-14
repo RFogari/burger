a38d05556483ee41df28ed5427e4f4616968ca87
rfogari <fogari10@gmail.com>
2018-09-13 22:53:49 -0400

    Updates to Orm.js

--- config/orm.js
+++ config/orm.js
@@ -42,7 +42,7 @@ var orm = {
     selectAll: function (table, cb) {
         var queryString = "SELECT * FROM " + table;
 
-        connection.query(queryString, function(err, result){
+        connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
             }
