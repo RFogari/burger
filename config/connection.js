699894980192a732493e6f61c2cce06f0511f1ec
rfogari <fogari10@gmail.com>
2018-09-19 20:15:31 -0400

    updated connection.js to correct database .env.

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABSE
+    database: process.env.DATABASE
 });
 
 
